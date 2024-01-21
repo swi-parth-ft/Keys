c27a100bd07a328b6fa4f3424cb842b969392dfa
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-01-20 21:15:05 -0500

    Initial Commit

---
+++ Keys/KeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeysApp.swift
+//  Keys
+//
+//  Created by Parth Antala on 2024-01-20.
+//
+
+import SwiftUI
+
+@main
+struct KeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
